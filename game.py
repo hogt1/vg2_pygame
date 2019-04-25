741bca4aa02c588b4755cf7fe5cec8eab56d627a
Tom Øyvind Hogstad <hogt1@skole.t-fk.no>
2019-04-25 08:16:23 +0200

    Pong x

--- game.py
+++ game.py
@@ -10,16 +10,24 @@ from settings import *
 import control # Importerer vår egen fil: control.py 
 
 #ball_angle = 0
-ball_dx = 0
-ball_dy = 0
+ball_dx = BALL_INITIAL_SPEED
+ball_dy = BALL_INITIAL_SPEED
 # Bruker et pygame.Rect for å holde kontroll på ballen
 ball_rect = pygame.Rect(DISPLAY_RESOLUTION[0]/2, 0, 50,50)
+
+p1_rect = pygame.Rect(0, DISPLAY_RESOLUTION[1]//2 - PADDLE_SIZE[1] // 2, PADDLE_SIZE[0], PADDLE_SIZE[1])
+# Setter Paddle 1 en bredde fra venstre
+p1_rect.left = PADDLE_SIZE[0]
+p2_rect = pygame.Rect(0, DISPLAY_RESOLUTION[1]//2 - PADDLE_SIZE[1] // 2, PADDLE_SIZE[0], PADDLE_SIZE[1])
+# Setter Paddle 2 en bredde fra høyre
+p2_rect.right = DISPLAY_RESOLUTION[0] - PADDLE_SIZE[0]
+
 
 
 def handle_event(event):
     global ball_dx
     global ball_dy
-    
+
     # Avslutter ved vindu [X]
     if (event.type == QUIT):
         pygame.quit()
@@ -27,19 +35,19 @@ def handle_event(event):
     # Et tastetrykk? Dokuemtasjon: https://www.pygame.org/docs/ref/key.html
     elif event.type == KEYDOWN:
         if event.key == K_UP:
-            ball_dy -= 1
+            p2_rect.top -= PADDLE_SPEED
         elif event.key == K_DOWN:
-            ball_dy += 1
-        elif event.key == K_LEFT:
-            ball_dx -= 1
-        elif event.key == K_RIGHT:
-            ball_dx += 1
+            p2_rect.top += PADDLE_SPEED
+        elif event.key == K_q:
+            p1_rect.top -= PADDLE_SPEED
+        elif event.key == K_a:
+            p1_rect.top += PADDLE_SPEED
 
 # Tegner ting
 def draw():
     # Tegner en hvit bakgrunn på "surface"
     # Dokumentasjon: https://www.pygame.org/docs/ref/surface.html#pygame.Surface.fill
-    surface.fill((255, 255, 255))
+    surface.fill((0, 0, 0))
     # Tegner en fylt sirkel
     # Dokuemtasjon: https://www.pygame.org/docs/ref/draw.html#pygame.draw.circle
     '''
@@ -50,6 +58,12 @@ def draw():
         int(ball_rect.width/2) # radius
         )
     '''
+    # Tegner Paddle 1
+    pygame.draw.rect(surface, PADDLE_COLOR, p1_rect)
+    # Tegner Paddle 2
+    pygame.draw.rect(surface, PADDLE_COLOR, p2_rect)
+
+
     # "Tegner" bilde
     surface.blit(ball_image, ball_rect)
 
@@ -64,12 +78,19 @@ def update():
 
     ball_rect.top += ball_dy
     ball_rect.left += ball_dx
-    if (ball_rect.right >= DISPLAY_RESOLUTION[0]) or (ball_rect.left <= 0):
+    if (ball_rect.right >= DISPLAY_RESOLUTION[0] - PADDLE_SIZE[0]):
+        pass
+    if (ball_rect.left <= 0):
         ball_dx = ball_dx*-1
-    
 
     if (ball_rect.bottom >= DISPLAY_RESOLUTION[1]) or (ball_rect.top <= 0):
         ball_dy = ball_dy*-1
+
+    if p1_rect.colliderect(ball_rect):
+        ball_dx = ball_dx*-1
+
+    if p2_rect.colliderect(ball_rect):
+        ball_dx = ball_dx*-1
 
     
     
--- settings.py
+++ settings.py
@@ -1,3 +1,7 @@
 # Instillinger for spillet
 # Størrelsen på vinduet til spillet
 DISPLAY_RESOLUTION = (800, 600)
+PADDLE_SIZE = (20, 80)
+PADDLE_COLOR = (0, 255, 0)
+PADDLE_SPEED = 6
+BALL_INITIAL_SPEED = 5
