a72ff00343951fab0e1dfe9c22813213dd45fc93
Tom Øyvind Hogstad <hogt1@skole.t-fk.no>
2019-05-07 11:24:25 +0200

    Noen flere kommentarer

--- settings.py
+++ settings.py
@@ -2,6 +2,10 @@
 # Størrelsen på vinduet til spillet
 DISPLAY_RESOLUTION = (800, 600)
 PADDLE_SIZE = (20, 80)
+# Farger er i RGB 
+# Her kan en evnt. teste RGB farger: https://www.w3schools.com/colors/colors_rgb.asp
 PADDLE_COLOR = (0, 255, 0)
+
+# Speed indikerer hvor mange pixler noe beveger seg.
 PADDLE_SPEED = 6
 BALL_INITIAL_SPEED = 5
